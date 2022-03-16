b06b6da4e98fbd21f4603a2a078380f31d478a27
Megan V <elbowgrrl@gmail.com>
2022-03-15 17:18:52 -0700

    adding footer

--- sphere-animations/src/App.js
+++ sphere-animations/src/App.js
@@ -4,11 +4,11 @@ function App() {
   const sphere3 = "images/sphere-3.png"
   return (
     <div className='container-outer'>
-    <div className='sphere-1'>sphere-1</div>
-    <div className='sphere-2'>sphere-2</div>
-    <div className='sphere-3'>sphere-3</div>
-    <div className='sphere-4'>sphere-4</div>
-    <div className='sphere-5'>sphere-5</div>
+    <div className='sphere-1'></div>
+    <div className='sphere-2'></div>
+    <div className='sphere-3'></div>
+    <div className='sphere-4'></div>
+    <div className='sphere-5'></div>
     </div>
   );
 }
